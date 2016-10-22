06cd7f03b8bd777f42178a4b3bee1a72c109f24f
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-21 18:02:06 -0700

    2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
 $('#game p').click(function(){
 	$('#game').html('<p>Who physically wrote the United States Constitution?</P>')
+	$('#game').append('<p>George Washington</p>')
+	$('#game').append('<p>Thomas Jefferson')
+	$('#game').append('<p>Gouverner Morris</p>')
 })
