8db0e27639b3d482f14e6404b4ec6756785d3194
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-22 20:27:16 -0700

    update 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,42 @@
-$('#game p').click(function(){
+$(document).ready(function(){
+var time;
+var questions = ["Who physically wrote the United States Constitution?", "When was George Washington born?", "Who was the Prime Minister during the revolutionary war?"]
+var answers = ["Gouverner Morris", "April 30th 1789", "The Earl of Guilford"]
+
+function stop(){
+	clearTimeout(time);
+}
+
+function change(){
+
+	var i = 0
+	$('#game p').one("click", function(){
+	$('#game').html(questions[0])
+	$('#game').append(answers[0])
+
+
+
+
+})
+	i++
+	if (i >= questions.length){
+
+	}
+
+}
+
+function setClock(){
+	time = setTimeout(change)
+}
+
+
+$('#game p').one("click", function(){
 	$('#game').html('<p>Who physically wrote the United States Constitution?</P>')
 	$('#game').append('<p>George Washington</p>')
 	$('#game').append('<p>Thomas Jefferson')
 	$('#game').append('<p>Gouverner Morris</p>')
+})
+
+
+
 })
