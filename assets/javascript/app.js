6975b9b342d24883eb2e5f16740a4d7369505cf5
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-24 21:00:05 -0700

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,53 @@
 $(document).ready(function(){
 var time;
-var questions = ["Who physically wrote the United States Constitution?", "When was George Washington born?", "Who was the Prime Minister during the revolutionary war?"]
-var answers = ["Gouverner Morris", "April 30th 1789", "The Earl of Guilford"]
+var questions = ["<p>Who physically wrote the United States Constitution?</p>", "<p>When was George Washington born?</p>", "<p>Who was the Prime Minister during the revolutionary war?</p>"];
+var answers = ["<p>Gouverner Morris<p>", "<p>April 30th 1789</p>", "<p>The Earl of Guilford</p>"];
+var displayTime = 30;
+
+function setClock(){
+	time = setInterval(updateTime, 1000);
+
+}
+
+function updateTime(){
+	displayTime -= 1;
+	$('#time').html(displayTime);
+	console.log(displayTime);
+}
+
+function rightAnswer(){
+	console.log("hi")
+	$('#game').html('<img src="assets/images/right.png">')
+}
 
 function stop(){
-	clearTimeout(time);
+	clearInterval(time);
 }
 
 function change(){
 
 	var i = 0
-	$('#game p').one("click", function(){
-	$('#game').html(questions[0])
-	$('#game').append(answers[0])
 
+	// $('#game').html(questions[i])
+	$('#game').append(answers[i])
 
+	$('#game p').on('click', function(){
+		rightAnswer();
+	})
 
-
-})
 	i++
 	if (i >= questions.length){
-
+		stop();
 	}
 
 }
 
-function setClock(){
-	time = setTimeout(change)
-}
+
 
 
 $('#game p').one("click", function(){
-	$('#game').html('<p>Who physically wrote the United States Constitution?</P>')
-	$('#game').append('<p>George Washington</p>')
-	$('#game').append('<p>Thomas Jefferson')
-	$('#game').append('<p>Gouverner Morris</p>')
+	change();
+	setClock();
 })
 
 
