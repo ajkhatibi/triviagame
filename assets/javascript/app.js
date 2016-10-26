d827c65ed1f36b45232ede7529d855106e57ac54
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-25 19:51:46 -0700

    update3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 var time;
 var questions = ["<p>Who physically wrote the United States Constitution?</p>", "<p>When was George Washington born?</p>", "<p>Who was the Prime Minister during the revolutionary war?</p>"];
 var answers = ["<p>Gouverner Morris<p>", "<p>April 30th 1789</p>", "<p>The Earl of Guilford</p>"];
+var wrongAnswers = {
+	question1: "george washington", "malcom x", "akbar the great"
+	question2: 
+}
 var displayTime = 30;
+var i = 0
 
 function setClock(){
 	time = setInterval(updateTime, 1000);
@@ -12,6 +17,9 @@ function setClock(){
 function updateTime(){
 	displayTime -= 1;
 	$('#time').html(displayTime);
+	if (displayTime === 0){
+		change();
+	}
 	console.log(displayTime);
 }
 
@@ -24,15 +32,20 @@ function stop(){
 	clearInterval(time);
 }
 
+function reset(){
+	displayTime = 30
+	stop():
+}
+
 function change(){
 
-	var i = 0
 
 	// $('#game').html(questions[i])
 	$('#game').append(answers[i])
 
 	$('#game p').on('click', function(){
 		rightAnswer();
+	$('#correct').hide();
 	})
 
 	i++
@@ -45,7 +58,8 @@ function change(){
 
 
 
-$('#game p').one("click", function(){
+$('#start').one("click", function(){
+	$('#start').hide();
 	change();
 	setClock();
 })
